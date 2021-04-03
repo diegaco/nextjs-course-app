e6c71f37323007dc3f7651218cdecb65798cbedb
Diego Acosta <diegoacosta211@gmail.com>
2021-04-02 22:13:12 -0300

    Dynamic Import

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,12 +2,21 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import Link from 'next/link';
+import dynamic from 'next/dynamic';
+
+const Ads = dynamic(
+  () => import('../src/components/Ads'),
+  {
+    loading: () => <p>Loading Ads...</p>
+  }
+)
 
 export default ({ content: { title } }) => {
   return (
   <div sx={{ height: `calc{ (100vh - 60px) }` }}>
     <div sx={{ variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%' }}>
         <h1 sx={{ fontSize: 8, my: 0, lineHeight: 1.1 }}>{ title }</h1>
+        <Ads/>
     </div>
   </div>
 ) };

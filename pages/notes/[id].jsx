14be17aa19d7714f590255d147651a07cd363c33
Diego Acosta <diegoacosta211@gmail.com>
2021-04-02 22:18:50 -0300

    change API_URL to be hosted in .env

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -15,7 +15,7 @@ export default ({ note }) => {
 }
 
 export async function getServerSideProps({ params, req, res }) {
-  const response = await fetch(`http://localhost:3000/api/notes/${params.id}`);
+  const response = await fetch(`${process.env.API_URL}/api/notes/${params.id}`);
 
  // so much power!
   if (!response.ok) {
